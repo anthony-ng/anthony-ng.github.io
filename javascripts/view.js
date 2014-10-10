e828affb3cd29baf7e0e123c854dd714c282006f
J. Ky Marsh <jkymarsh@gmail.com>
2014-10-10 12:07:07 -0700

    Begin adding logic for orange.html page

--- javascripts/view.js
+++ javascripts/view.js
@@ -1,5 +1,26 @@
 var View = function() {
     this.el = document.getElementsByClassName('grove')[0];
+
+    this.initialize();
+};
+
+View.prototype.initialize = function() {
+    console.log('haha');
+
+    this.attachEventListeners();
+};
+
+View.prototype.attachEventListeners = function() {
+    var that = this,
+        plantButton = document.getElementById('plant-tree');
+
+    if (plantButton) {
+        plantButton.addEventListener('click', function() {
+            var tree = new Tree();
+
+            that.renderTree(tree);
+        });
+    }
 };
 
 View.prototype.renderTree = function(tree) {
