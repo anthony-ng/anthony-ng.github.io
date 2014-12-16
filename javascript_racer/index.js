ee331f89d4c0088147b4199fe0ab77389a6b1cfc
Anthony Ng <thomsonng123@gmail.com>
2014-12-15 17:40:06 -0800

    update version 1.3, refactor codes

--- javascript_racer/index.js
+++ javascript_racer/index.js
@@ -1,31 +1,27 @@
-// Models
-var player1 = document.getElementById("player1_strip");
-var player2 = document.getElementById("player2_strip");
-// console.log("its moving!")
 
 var move = function(e) {
+
+var active1 = document.querySelector(".active1")
+var active2 = document.querySelector(".active2")
+
   if (e.keyCode == 81) {
-    var active1 = player1.getElementsByClassName("active1")[0];
     if (active1.classList.contains("finish-line")){
-      alert("Player 1 wins!")
-      var next = active1.nextElementSibling.classList.add("active1");
-      active1.classList.remove("active1");
+      alert("Eat my short! Donut Eater!")
+      window.location.reload()
     }
     else {
-      next = active1.nextElementSibling.classList.add("active1");
+      active1.nextElementSibling.classList.add("active1");
       active1.classList.remove("active1");
   }}
   else if (e.keyCode == 80) {
-    var active2 = player2.getElementsByClassName("active2")[0];
     if (active2.classList.contains("finish-line")){
-      alert("Player 2 wins!")
-      var next = active2.nextElementSibling.classList.add("active2");
-      active2.classList.remove("active2");
+      alert("I caught you! Son!")
+      window.location.reload()
     }
     else {
-      next = active2.nextElementSibling.classList.add("active2");
+      active2.nextElementSibling.classList.add("active2");
       active2.classList.remove("active2");
   }}
 }
 
-window.addEventListener("keydown", move);
+window.addEventListener("keyup", move);
